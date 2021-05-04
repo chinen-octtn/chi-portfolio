b07c3edbb5c9bd927f89b959b40b7e60774c0142
maashi <octtn8c@gmail.com>
2021-05-04 19:28:42 +0900

    add: テキストパーツ

---
+++ src/components/element/Text.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="text">{{ text }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.text {
+  color: var(--color-text);
+}
+</style>
