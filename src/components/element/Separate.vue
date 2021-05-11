bd4ec806dbfd941a7c3c42c34f81913eab490f77
maashi <octtn8c@gmail.com>
2021-05-11 17:49:25 +0900

    add: Separater

---
+++ src/components/element/Separate.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :style="`margin-top: ${separate}px;`">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  setup(props) {
+    const separate = props.size * 16
+
+    return {
+      separate,
+    }
+  },
+}
+</script>
